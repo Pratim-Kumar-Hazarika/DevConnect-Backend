5a53ce9c2ac3fb55cbd572da492d593a301e13ba
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-09-13 23:06:54 +0530

    Create user.model.js

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    name :{
+    type:String,
+    required:[true,"Name is needed "]
+  },
+    username :{
+    type:String,
+    required:[true,"Username is needed "]
+  },
+  email:{
+    type:String,
+    unique:true,
+    required:true,
+     validate: {
+      validator: function(v) {
+        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+      },
+      message: props => `${props.value} is not a valid email id!`
+  }},
+  password:{
+    type:String,
+    required:true
+  },
+})
+
+const User = mongoose.model("User",UserSchema);
+
+module.exports = { User };
