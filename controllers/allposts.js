c93a0dc663b6648c8853f30377058483a13ef9c9
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-05 00:18:28 +0530

    update : populate user_id

--- controllers/allposts.js
+++ controllers/allposts.js
@@ -2,7 +2,9 @@ const { UserPost } = require("../models/posts.model")
 
 exports.all_posts = async(req,res)=>{ //ALL posts for the feed
   try{
-      const posts = await UserPost.find({}).populate("posts.likes posts.comments._id")
+      const posts = await UserPost.find({}).populate("posts.likes posts.comments._id _id")
+      ///Build this later 
+      ///filter out necessary details and send to client
       res.json({message:"All posts for the feed are..",posts})
   }catch{
     res.status(500).json({message:"error in getting the user post.."})
