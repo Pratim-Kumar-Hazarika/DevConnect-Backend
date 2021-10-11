529fa4141393f9021bbfb02e1e0bc52c54d435f5
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2021-10-12 00:09:39 +0530

    add : multiple comments like children or parent to a particular post

--- controllers/user.post.comments.js
+++ controllers/user.post.comments.js
@@ -1,32 +1,69 @@
-const { UserPost } = require("../models/posts.model");
+const {UserPost} = require("../models/posts.model");
 
-exports.add_comment = async(req,res)=>{
+exports.add_comment = async(req, res) => {
     try {
         const {decodedValues} = req.user;
         const {postId} = req.body;
-        await UserPost.updateOne({"_id":decodedValues.userId,"posts._id":postId},{
-            "$push":{
-                "posts.$.comments":{"_id":req.body.commenterId,"text":req.body.text,"commentorName":decodedValues.name}
-            }
-        })
-        res.json({message:"Comment Successfull!!"})
-        
+        ///For children comments
+        if (req.body.childrenComment) {
+            await UserPost.updateOne({
+                "_id": req.body.postBelongingToUserId,
+                "posts._id": postId
+            }, {
+                "$push": {
+                    "posts.$[].comments.$[elem].childrenComments": {
+                        "_id": req.body.commenterId, ///come from token
+                        "comment": req.body.childrenComment
+                    }
+                }
+            }, {
+                "arrayFilters": [
+                    {
+                        "elem._id": req.body.parrentCommentId
+                    }
+                ],
+                multi: true
+            })
+        }else{
+            ///For parrent Comment
+            await UserPost.updateOne({
+                "_id": req.body.postBelongingToUserId,
+                "posts._id": postId
+            },{
+                "$push":{
+                    "posts.$.comments":{
+                        "_id": decodedValues.userId,
+                        "parrentComment": req.body.parrentComment
+                    }
+                }
+            })
+        }
+
+        res.json({message: "Comment Successfull!!"})
+
     } catch (error) {
-        res.status(500).json({message:"Comment Unsucessfull "})
+        res .status(500).json({message: "Comment Unsucessfull "})
     }
 }
 
-exports.delete_comment = async(req,res)=>{ //Delete Comment
-    try{
+exports.delete_comment = async(req, res) => { //Delete Comment //Build this later
+    try {
         const {decodedValues} = req.user;
         const {postId} = req.body;
-         await UserPost.updateOne({"_id":decodedValues.userId,"posts._id":postId},{
-           "$pull":{
-               "posts.$.comments": {"_id": req.body.commenterId}
-           }
-         })
-         res.json({message:"Comment deleted sucessfully"})
-    }catch{
-      res.status(500).json({message:"Comment was not deleted"})
+        await UserPost.updateOne({
+            "_id": decodedValues.userId,
+            "posts._id": postId
+        }, {
+            "$pull": {
+                "posts.$.comments": {
+                    "_id": req.body.commenterId
+                }
+            }
+        })
+        res.json({message: "Comment deleted sucessfully"})
+    } catch {
+        res
+            .status(500)
+            .json({message: "Comment was not deleted"})
     }
-  }
+}
